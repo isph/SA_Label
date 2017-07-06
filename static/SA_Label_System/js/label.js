0d95fe0208726698f9b93b90c5dd4289d5bfe87c
isph <isph@foxmail.com>
2017-07-06 17:14:28 +0800

    Delete some files

--- static/SA_Label_System/js/label.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sph on 7/2/17.
- */
-var cnt = 0;
-function rotate_label_image() {
-    console.log(cnt);
-    cnt = (cnt+1)%4;
-    document.getElementById("label_image").style.transform="rotate("+cnt*90+"deg)";
-}
